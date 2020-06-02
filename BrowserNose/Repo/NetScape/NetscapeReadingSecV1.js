a1ce0ef721bfcdb58635cdeae4f47429d3cc4154
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-01 18:55:41 -0700

    NetscapeReadingSecV1.js
    Version 1 of the Netscape Browser browser sniffing script.

---
+++ BrowserNose/Repo/NetScape/NetscapeReadingSecV1.js
@@ -0,0 +1,76 @@
+<script type="text/JavaScript" lang="en">
+// Netscape/Netscape Navigator/Netscape Browser/Netscape communicator page compatibility
+console.log("Netscape browser sniffing script");
+// Netscape 1.x
+console.log("Unable to sniff for NetScape 1.x, no versions available");
+// Netscape 2.x
+console.log("Unable to sniff for NetScape 2.x, no versions available");
+// Netscape 3.x
+console.log("Unable to sniff for NetScape 3.x, no versions available");
+// Netscape 4.x
+console.log("Unable to sniff for NetScape 4.x, no versions available");
+// Netscape 5.x
+console.log("Unable to sniff for NetScape 5.x, no versions available");
+// Netscape 6.x
+console.log("Unable to sniff for NetScape 6.x, no versions available");
+// Netscape 7.x
+/* NetScape 7.0 Preview Release 1 (PR1) page reading section */
+function sniffNetScape7_0_PR1_func {
+	console.log("Sniffing browser to check Netscape version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Netscape 7 Preview Release 1");
+	} else {
+		console.log("Sniffing completed! Netscape 7 PR1  has been detected with UA (Unknown");
+}}
+/* NetScape 7.0 page reading section */
+function sniffNetScape7_0_func {
+	console.log("Sniffing browser to check Netscape version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Netscape 7");
+	} else {
+		console.log("Sniffing completed! Netscape 7 has been detected with UA (Unknown");
+}}
+/* NetScape 7.01 page reading section */
+function sniffNetScape7_0_1_func {
+	console.log("Sniffing browser to check Netscape version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Netscape 7.01");
+	} else {
+		console.log("Sniffing completed! Netscape 7.01 has been detected with UA (Unknown");
+}}
+/* NetScape 7.02 page reading section */
+function sniffNetScape7_0_2_func {
+	console.log("Sniffing browser to check Netscape version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Netscape 7.02");
+	} else {
+		console.log("Sniffing completed! Netscape 7.02 has been detected with UA (Unknown");
+}}
+/* NetScape 7.1 page reading section */
+function sniffNetScape7_1_0_func {
+	console.log("Sniffing browser to check Netscape version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Netscape 7.1");
+	} else {
+		console.log("Sniffing completed! Netscape 7.1 has been detected with UA (Unknown");
+}}
+/* NetScape 7.2 page reading section */
+function sniffNetScape7_2_0_func {
+	console.log("Sniffing browser to check Netscape version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Netscape 7.2");
+	} else {
+		console.log("Sniffing completed! Netscape 7.2 has been detected with UA (Unknown");
+}}
+// Netscape 8.x
+console.log("Unable to sniff for NetScape 7.x, no versions available");
+// Netscape 9.x
+console.log("Unable to sniff for NetScape 8.x, no versions available");
+// Script version: 1 (March 5th 2020)
+</script>
