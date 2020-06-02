e95c09d01899211671f3c875cfe6089564a57212
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-01 18:42:54 -0700

    AvantBrowserReadingSecV1.js
    Version 1 of the Avant browser browser sniffing script.

---
+++ BrowserNose/Repo/AvantBrowser/AvantBrowserReadingSecV1.js
@@ -0,0 +1,5 @@
+<script type="text/JavaScript" lang="en">
+// AvantBrowser page compatibility
+console.log("No reference to a version list is available for the AvantBrowser. Unable to sniff");
+// Script version: 1 (March 5th 2020)
+</script>
