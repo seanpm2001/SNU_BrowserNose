18c63c0ad2055564b05c834db1b21c2da8407fe1
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-01 18:43:30 -0700

    BraveReadingSecV1.js
    Version 1 of the Brave browser browser sniffing script.

---
+++ BrowserNose/Repo/Brave/BraveReadingSecV1.js
@@ -0,0 +1,5 @@
+<script type="text/JavaScript" lang="en">
+// Brave page compatibility
+console.log("No reference to a version list is available for the Brave Browser. Unable to sniff");
+// Script version: 1 (March 7th 2020)
+</script>
