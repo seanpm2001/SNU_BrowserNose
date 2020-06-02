dcb13589caf96cd2a22b921d91b5ba239b8a633a
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-01 18:44:00 -0700

    ClassillaReadingSecV1.js
    Version 1 of the Classilla browser browser sniffing script.

---
+++ BrowserNose/Repo/Classilla/ClassillaReadingSecV1.js
@@ -0,0 +1,5 @@
+<script type="text/JavaScript" lang="en">
+// Classilla page compatibility
+console.log("No reference to a version list is available for the Classilla Browser. Unable to sniff");
+// Script version: 1 (March 8th 2020)
+</script>
