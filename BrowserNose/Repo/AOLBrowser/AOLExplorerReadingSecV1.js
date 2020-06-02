5add5797558efa136d0e9c785862daeee28d49c4
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-01 18:42:20 -0700

    AOLExplorerReadingSecV1.js
    Version 1 of the AOL Explorer browser sniffing script.

---
+++ BrowserNose/Repo/AOLBrowser/AOLExplorerReadingSecV1.js
@@ -0,0 +1,14 @@
+<script type="text/JavaScript" lang="en">
+// AOL Explorer/AOL Browser page compatibility
+console.log("Browser sniffing for AOL explorer/AOL browser");
+/* AOL Explorer 1.5 page reading section */
+function sniffAOLBrowserExplorer_1_5_func {
+	console.log("Sniffing browser to check AOL Explorer version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not AOL Explorer 1.5");
+	} else {
+		console.log("Sniffing completed! AOL Explorer 1.5 has been detected with UA (Unknown");
+}}
+// Script version: 1 (March 5th 2020)
+</script>
