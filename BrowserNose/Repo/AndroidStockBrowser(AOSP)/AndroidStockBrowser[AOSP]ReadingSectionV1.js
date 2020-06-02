2e24a46becbb4f770c9eea6845b8fca54c6b00e7
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-01 17:52:48 -0700

    AndroidStockBrowser[AOSP]ReadingSectionV1.js
    The first version of the Android stock browser browser sniffing script. Still in non-working draft form.
    Note: the Android Stock Browser is NOT Google Chrome or Chromium, it shipped with Android phones prior to version 5.1. It is WebKit based though, Chromium/Google Chrome are built off of Apples WebKit.

---
+++ BrowserNose/Repo/AndroidStockBrowser(AOSP)/AndroidStockBrowser[AOSP]ReadingSectionV1.js
@@ -0,0 +1,48 @@
+<script type="text/javascript" lang="en">
+// Android Stock Browser (AOSP) support for SNU
+/* VER UNKNOWN */ 
+console.log("Google Sniff kit is unable to run, unsorted scripts");
+var navU = navigator.userAgent;
+// Android Mobile
+var isAndroidMobile = navU.indexOf('Android') > -1 && navU.indexOf('Mozilla/5.0') > -1 && navU.indexOf('AppleWebKit') > -1;
+console.log("General Android Mobile sniff in progress");
+// Apple webkit
+/* Fork
+var regExAppleWebKit = new RegExp(/AppleWebKit\/([\d.]+)/);
+var resultAppleWebKitRegEx = regExAppleWebKit.exec(navU);
+var appleWebKitVersion = (resultAppleWebKitRegEx === null ? null : parseFloat(regExAppleWebKit.exec(navU)[1]));
+*/
+// Chrome 
+/* Fork
+var regExChrome = new RegExp(/Chrome\/([\d.]+)/);
+var resultChromeRegEx = regExChrome.exec(navU);
+var chromeVersion = (resultChromeRegEx === null ? null : parseFloat(regExChrome.exec(navU)[1]));
+*/
+// Native Android Browser
+var isAndroidBrowser = isAndroidMobile && (appleWebKitVersion !== null && appleWebKitVersion < 537) || (chromeVersion !== null && chromeVersion < 37);
+console.log(" ");
+/* User agent strings */
+// Source: https://stackoverflow.com/questions/14403766/how-to-detect-the-stock-android-browser#19915132
+// "Android Browser" is the name of a browser. Confusing perhaps, but with a question like this, it shouldn't be. It's the browser that comes with the vast majority of Android devices, and he's obviously not talking about Chrome.
+// According to http://www.useragentstring.com/ this is the full list of Android Stock Browser strings. I am currently unsure whether there is a better way to select all of them at once, since some will continue to be added, and also which would not accidentally target Chrome on Android. Hopefully someone gives you a better answer as I am curious myself of the best way to go about this.
+/*
+Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30
+Mozilla/5.0 (Linux; U; Android 4.0.3; de-ch; HTC Sensation Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30
+Mozilla/5.0 (Linux; U; Android 2.3; en-us) AppleWebKit/999+ (KHTML, like Gecko) Safari/999.9
+Mozilla/5.0 (Linux; U; Android 2.3.5; zh-cn; HTC_IncredibleS_S710e Build/GRJ90) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+Mozilla/5.0 (Linux; U; Android 2.3.5; en-us; HTC Vision Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+Mozilla/5.0 (Linux; U; Android 2.3.4; fr-fr; HTC Desire Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; T-Mobile myTouch 3G Slide Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+Mozilla/5.0 (Linux; U; Android 2.3.3; zh-tw; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+Mozilla/5.0 (Linux; U; Android 2.3.3; zh-tw; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari
+Mozilla/5.0 (Linux; U; Android 2.3.3; zh-tw; HTC Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+Mozilla/5.0 (Linux; U; Android 2.3.3; ko-kr; LG-LU3000 Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+Mozilla/5.0 (Linux; U; Android 2.3.3; en-us; HTC_DesireS_S510e Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+Mozilla/5.0 (Linux; U; Android 2.3.3; en-us; HTC_DesireS_S510e Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile
+Mozilla/5.0 (Linux; U; Android 2.3.3; de-de; HTC Desire Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+Mozilla/5.0 (Linux; U; Android 2.3.3; de-ch; HTC Desire Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+Mozilla/5.0 (Linux; U; Android 2.2; fr-lu; HTC Legend Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+Mozilla/5.0 (Linux; U; Android 2.2; en-sa; HTC_DesireHD_A9191 Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+*/
+// Script version 1 - February 27th 2020
+</script>
