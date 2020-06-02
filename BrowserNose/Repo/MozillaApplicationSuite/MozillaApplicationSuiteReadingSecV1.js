6a033875e69aefb049685ee05479d7aa473b9c9c
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-01 18:53:59 -0700

    MozillaApplicationSuiteReadingSecV1.js
    Version 1 of the Mozilla Application Suite browser browser sniffing script.

---
+++ BrowserNose/Repo/MozillaApplicationSuite/MozillaApplicationSuiteReadingSecV1.js
@@ -0,0 +1,5 @@
+<script type="text/JavaScript" lang="en">
+// Mozilla Application Suite page compatibility
+console.log("No reference to a version list is available for the Mozilla Application Suite Browser. Unable to sniff");
+// Script version: 1 (March 10th 2020)
+</script>
