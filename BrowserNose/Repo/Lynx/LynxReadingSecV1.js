4cd5170c42a0510fec1b469013f4c8f3b895966d
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-01 18:51:21 -0700

    LynxReadingSecV1.js
    Version 1 of the Lynx browser browser sniffing script.

---
+++ BrowserNose/Repo/Lynx/LynxReadingSecV1.js
@@ -0,0 +1,5 @@
+<script type="text/JavaScript" lang="en">
+// Lynx page compatibility
+console.log("No reference to a version list is available for the Lynx Browser. Unable to sniff");
+// Script version: 1 (March 5th 2020)
+</script>
