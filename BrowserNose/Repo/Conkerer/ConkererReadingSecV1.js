6920596d5243e8cf7c9a28acd02ec1a0c9eb80fc
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-01 18:44:46 -0700

    ConkererReadingSecV1.js
    Version 1 of the Conkerer browser browser sniffing script. Not to be confused with the Konquerer browser.

---
+++ BrowserNose/Repo/Conkerer/ConkererReadingSecV1.js
@@ -0,0 +1,13 @@
+<script type="text/JavaScript" lang="en">
+// Conkerer page compatibility
+console.log("Conkerer browser sniffing in progress...");
+function sniffConkerer_1_0_4_func {
+	console.log("Sniffing browser to check Conkerer version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Conkerer 1.04");
+	} else {
+		console.log("Sniffing completed! Conkerer 1.04 has been detected with UA (Unknown");
+}}
+// Script version: 1 (March 6th 2020)
+</script>
