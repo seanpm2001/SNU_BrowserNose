ac648071c77f7c6363949cb03fb6ecd0797049e9
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-01 18:52:35 -0700

    MicrosoftEdgeReadingSectionV1.js
    Version 1 of the Microsoft Edge browser browser sniffing script.

---
+++ BrowserNose/Repo/MicrosoftEdge/MicrosoftEdgeReadingSectionV1.js
@@ -0,0 +1,323 @@
+<script type="text/JavaScript" lang="en">
+// Microsoft Edge page compatibility
+/* Split into 2 parts:
+EdgeHTML engine (2015-2019)
+Chromium engine (2019-2020)
+*/
+// EdgeHTML section
+/* Microsoft Edge 20.10240 page reading section */
+/* Release notes
+Released: July 15th 2015
+Link: unavailable
+First public release, initial release for PC
+
+		Support for PDFs
+		Pinnable Hub with Favorites, Download manager, Reading List and History
+		New Tab page with MSN news and search bar
+		Support for inking on webpages
+		Cortana Integration
+		Reading View
+		Dark theme
+		Performance improvements
+*/
+function sniffMicrosoftEdge_20_10240_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 20.10240 (EdgeHTML)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 20.10240 (EdgeHTML) has been detected with UA (Unknown");
+}}
+// MicroSoft edge 21-24 do not exist
+/* Microsoft Edge 25.10240 page reading section */
+/* 
+Release notes
+Released: November 5th 2015
+Link: unavailable
+Initial release on Windows 10 Mobile and Xbox One
+
+		Improved render engine with Pointer Lock support, canvas blending modes, asm.js support by default and more
+		Object RTC API
+		Improved tab management
+		Tabs can now be previewed by hovering over them
+		Reading list items and favorites are now synced
+		Updated Settings pane
+    Hub options are now available in the ellipses menu
+    Media Casting
+    Cortana Integration with PDFs
+*/
+function sniffMicrosoftEdge_25_10586_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 25.10586 (EdgeHTML)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 25.10586 (EdgeHTML) has been detected with UA (Unknown");
+}}
+// Microsoft Edge 26-37 do not exist
+/* Microsoft Edge 38.14393 page reading section */
+/*
+Release notes
+Released: August 2nd 2016
+Link: unavailable
+Initial release on Windows Holographic
+
+		Experimental support for VP9
+		Context menu for navigation buttons
+		Improved favorites management
+		Improved download management
+		Pinned tab support added
+		Ability to copy and paste links in Microsoft Edge added
+		Download reminders
+		Default save locations can be changed
+		Favorite Tree View improvements
+		Experimental JavaScript ES6 Regex symbols support
+		New Web Platform features
+		Improved F12 developer tools
+		Improved accessibility
+		Swipe gestures to navigate back and forward
+		Support for Beacon, Web Notifications and Fetch API
+		Official support for browser extensions
+		WOFF 2 fonts
+		Support for Color Fonts formats (sbix, OpenType-SVG, COLR/CPAL, CBDT/CBLC)
+*/
+function sniffMicrosoftEdge_38_14393_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 38.14393 (EdgeHTML)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 38.14393 (EdgeHTML) has been detected with UA (Unknown");
+}}
+/* Microsoft Edge 40.15063 page reading section */
+/*
+Release notes
+Link: unavailable
+Released: April 11th 2017
+		Added CTRL + O keyboard shortcut to set focus to the address bar
+		Partial implementation of Webkit-Text-Stroke and CSS outline-offset
+		Ability to use Snooze to put a website in a Cortana Reminder to make it show up in the Action Center
+		Ability to import and export favorites from and to a file
+		Improved ES6 Modules debugging in F12 Developer Tools
+		H.264/AVC became enabled by default for RTC
+		Support for WebRTC 1.0 and Service Workers (behind flags)
+		Added support for the EPUB file format
+		The "Snooze" feature has been removed
+		Icons of pages in the hub became larger
+		Console filter settings will persist for buttons and context menu
+		Support for the Brotli compressed data format as an HTTP content-encoding method
+		Updated the MS-prefixed FIDO 2.0 implementation to match the latest W3C Web Authentication specification
+		Partial support for CSS Custom Properties (aka CSS Variables)
+		Preliminary support for the IntersectionObserver API
+		Async/await is enabled by default
+		DOM performance improvements
+		Advanced Tab Management
+		Added support for EPUB/PDF read aloud
+*/
+function sniffMicrosoftEdge_40_15063_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 40.15063 (EdgeHTML)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 40.15063 (EdgeHTML) has been detected with UA (Unknown");
+}}
+/* Microsoft Edge 41.16299 page reading section */
+/*
+Release notes
+Link: unavailable
+Released: September 26th 2017
+		WebAssembly now enabled by default
+*/
+function sniffMicrosoftEdge_41.16299_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 41.16299 (EdgeHTML)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 41.16299 (EdgeHTML) has been detected with UA (Unknown");
+}}
+/* Microsoft Edge 42.17134 page reading section */
+/*
+Release notes
+Link: unavailable
+Released: April 30th 2018
+		Support for Progressive Web Apps
+		CSS transforms on SVG elements
+		Support for Notification API
+*/
+function sniffMicrosoftEdge_42.17134_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 42.17134 (EdgeHTML)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 42.17134 (EdgeHTML) has been detected with UA (Unknown");
+}}
+/* Microsoft Edge 44.17763 page reading section */
+/*
+Release notes
+Link: unavailable
+Released: November 13th 2018
+		Support for Autoplay Policies
+		CSS Masking, overflow-wrap and overscroll-behavior support
+		Improvements to Developer Tools
+		WebP image format support
+		Web Authentication API support
+		High-quality kerning pairs and ligatures
+		Promise.prototype.finally support
+		Remove support for e-books that use the EPUB
+*/
+function sniffMicrosoftEdge_44.17763_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 44.17763 (EdgeHTML)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 44.17763 (EdgeHTML) has been detected with UA (Unknown");
+}}
+/* Microsoft Edge 44.18362 page reading section */
+/*
+Release notes
+Link: unavailable
+Released: May 21st 2019
+		Support for Autoplay Policies
+		CSS Masking, overflow-wrap and overscroll-behavior support
+		Improvements to Developer Tools
+		WebP image format support
+		Web Authentication API support
+		High-quality kerning pairs and ligatures
+		Promise.prototype.finally support
+		Remove support for e-books that use the EPUB
+*/
+function sniffMicrosoftEdge_44.18362_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 44.17362 (EdgeHTML)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 44.17362 (EdgeHTML) has been detected with UA (Unknown");
+}}
+/* Microsoft Edge 44.18363 page reading section */
+/*
+Release notes
+Link: unavailable
+Released: November 12th 2019
+		Support for Autoplay Policies
+		CSS Masking, overflow-wrap and overscroll-behavior support
+		Improvements to Developer Tools
+		WebP image format support
+		Web Authentication API support
+		High-quality kerning pairs and ligatures
+		Promise.prototype.finally support
+		Remove support for e-books that use the EPUB
+*/
+function sniffMicrosoftEdge_44.18363_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 44.17363 (EdgeHTML)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 44.17363 (EdgeHTML) has been detected with UA (Unknown");
+}}
+/* Microsoft Edge 44.19041 page reading section */
+/*
+Release notes
+Link: unavailable
+Released: 2020 (no further date info found)
+		Support for Autoplay Policies
+		CSS Masking, overflow-wrap and overscroll-behavior support
+		Improvements to Developer Tools
+		WebP image format support
+		Web Authentication API support
+		High-quality kerning pairs and ligatures
+		Promise.prototype.finally support
+		Remove support for e-books that use the EPUB
+*/
+function sniffMicrosoftEdge_44.19041_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 44.19041 (EdgeHTML)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 44.18041 (EdgeHTML) has been detected with UA (Unknown");
+}}
+// The last 4 EdgeHTML versions contain/share the same documentation, so the info was copied thrice
+// Chromium section
+/* Microsoft Edge 79.0.309 page reading section */
+/*
+Release notes
+Link: unavailable
+Released: January 15th 2020
+Initial release of the Chromium-based version
+
+			Support for Windows 7 and 8.1, Windows Server 2008 R2, 2012, 2012 R2, 2016 and 2019 and macOS
+*/
+function sniffMicrosoftEdge_79_0_309_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 79.0.309 (Chromium)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 79.0.309 (Chromium) has been detected with UA (Unknown");
+}}
+/* Microsoft Edge 80.0.361 page reading section */
+/*
+Release notes
+Link: unavailable
+Released: February 7th 2020
+		Native support for ARM processors
+		The address bar now shows when a website is using the microphone and camera
+		Adds warnings for potentially dangerous downloads
+		Extensions will now show who published them
+		Support for restoring sessions of websites that have been installed as apps after an update or crash
+		Adds an option to the context menu to sort favorite folders by name
+		Adds a new setting to always use Strict Tracking Prevention inside InPrivate windows
+		Extensions that influence the browser's appearance can now be installed from the Chrome Web Store
+		Support for importing Firefox browser history
+*/
+function sniffMicrosoftEdge_80_0_361_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 80.0.361 (Chromium)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 80.0.361 (Chromium) has been detected with UA (Unknown");
+}}
+/* Microsoft Edge 81.0.410 page reading section */
+/*
+Release notes
+Link: unavailable
+Released: To Be Announced
+		Adds keyboard shortcuts to access Read Aloud and the Immersive Reader
+		Option to hide the Collections button
+		Improved favorites management with tools to remove duplicate favorites
+		Edge will now show a message when another app installs an extension in Edge
+		Support for Windows built-in spellchecker
+		Support for using cards that have been saved in MSPay to be used on webpages
+		Dolby Vision support
+		Support for Microsoft Information Protection-enabled PDFs
+		3D viewer in the F12 Developer Tools
+		Adds a setting to enable or disable automatic profile switching
+		Shows a dialog whenever a new data type is available to sync (like history and extensions)
+		The InPrivate button now uses a blue background
+		Support for highlighting text in PDF, support for web-optimized PDFs that are downloaded on page at a time as they are being viewed
+		Adds a page to settings to inform users on how to install Edge on their phone
+*/
+function sniffMicrosoftEdge_81_0_410_func {
+	console.log("Sniffing browser to check Microsoft Edge version");
+	var sniffo1 = 0;
+	if sniffo1 = 1 {
+		console.log("This is not Microsoft Edge 81.0.410 (Chromium)");
+	} else {
+		console.log("Sniffing completed! Microsoft Edge 81.0.410 (Chromium) has been detected with UA (Unknown");
+}}
+// This release is coming soon, data may need to be updated upon release
+// Sources:
+/* Wikipedia:
+(EdgeHTML releases) https://en.wikipedia.org/wiki/Microsoft_Edge#Spartan_(2014%E2%80%932019)
+(Chromium releases) https://en.wikipedia.org/wiki/Microsoft_Edge#Anaheim_(2019%E2%80%93present)
+(Main) https://en.wikipedia.org/wiki/Microsoft_Edge#Release_history
+*/
+// Script version 1 - February 29th 2020
+</script>
