3c47e4132c1ec6ce5ee38db84c9f8341e5b36abe
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-05 19:40:51 -0800

    V1.deps.js
    An archive of the first version of the dependencies file for this project.

---
+++ OldVersions/.deps.js/1/1-100/V1.deps.js
@@ -0,0 +1,55 @@
+// Dependencies for SNU_BrowserNose
+
+// HTML version: HTML5 Preferred for modern pages, older variants available for older versions
+// CSS version: 3.0 or later for modern pages, older variants available for older versions
+// JavaScript version: 1.8 or later for modern pages, older variants available for older versions
+// Markdown (CommonMark)
+// GNU Make version: 1.0 or later
+// JSON version: Not set
+// INI version: Not set
+// .Desktop file version: Not set
+// LaTeX version: 2.0 or later
+// YAML version: 1.1
+// SVG version: 1.1 or later for modern pages, older variants available for older versions
+// PNG version: Any
+// JPEG Version: Any
+// GIF version: 1.1 or later for modern pages, older variants available for older versions
+// Web browser: Any
+// XML version: 1.1 or later for modern pages, older variants available for older versions
+// BitMap version: Any
+// Git version: 1.0 or later
+// Emoji version: 10.0 or later for modern pages, older variants available for older versions
+// CFF version: 1.2.0
+// BASH shell version: 1.0 or later
+// ReStructured Text version: 1.0 or later
+// MediaWiki text version: 5.0 or later
+
+console.log("HTML version: HTML5 Preferred for modern pages, older variants available for older versions");
+console.log("CSS version: 3.0 or later for modern pages, older variants available for older versions");
+console.log("JavaScript version: 1.8 or later for modern pages, older variants available for older versions");
+console.log("Markdown (CommonMark)");
+console.log("GNU Make version: 1.0 or later");
+console.log("JSON version: Not set");
+console.log("INI version: Not set");
+console.log(".Desktop file version: Not set");
+console.log("LaTeX version: 2.0 or later");
+console.log("YAML version: 1.1");
+console.log("SVG version: 1.1 or later for modern pages, older variants available for older versions");
+console.log("PNG version: Any");
+console.log("JPEG Version: Any");
+console.log("GIF version: 1.1 or later for modern pages, older variants available for older versions");
+console.log("Web browser: Any");
+console.log("XML version: 1.1 or later for modern pages, older variants available for older versions");
+console.log("BitMap version: Any");
+console.log("Git version: 1.0 or later");
+console.log("Emoji version: 10.0 or later for modern pages, older variants available for older versions");
+console.log("CFF version: 1.2.0");
+console.log("BASH shell version: 1.0 or later");
+console.log("ReStructured Text version: 1.0 or later");
+console.log("MediaWiki text version: 5.0 or later");
+break;
+
+// File info
+// File type: JavaScript source file (*.js)
+// File version: 1 (2022, Saturday, March 5th at 7:40 pm)
+// Line count (including blank lines and compiler line): 56
